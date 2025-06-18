aa8c755978da1cd9a620af8a2e5c5174a16107ec
aimeaimeaime <babineaime2004@gmail.com>
2025-06-18 20:49:32 +0100

    mise à jour du projet web antivAjout de la carte avec pointeur pour la localisationl

--- firebase.js
+++ firebase.js
@@ -1,3 +1,22 @@
+// /// firebase.js
+// const { initializeApp } = require('firebase/app');
+// const { getDatabase, ref, onValue } = require('firebase/database');
+
+// const firebaseConfig = {
+//   databaseURL: 'https://systemeantivol-4bcbf-default-rtdb.firebaseio.com'
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const db = getDatabase(app);
+
+// module.exports = { db, ref, onValue };
+
+
+
+
+
+
+
 /// firebase.js
 const { initializeApp } = require('firebase/app');
 const { getDatabase, ref, onValue } = require('firebase/database');
--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+// /// index.js
+// const express = require('express');
+// const cors = require('cors');
+// const { db, ref, onValue } = require('./firebase');
+// const path = require('path');
+
+// const app = express();
+// const PORT = process.env.PORT || 10000;
+
+// app.use(cors());
+// app.use(express.static('public'));
+
+// app.get('/location/:email', (req, res) => {
+//   const rawEmail = req.params.email;
+//   const emailKey = rawEmail.replace(/\./g, '_');
+
+//   const userRef = ref(db, `localisation/${emailKey}`);
+
+//   onValue(userRef, (snapshot) => {
+//     const data = snapshot.val();
+//     if (data) {
+//       const latest = Object.values(data).pop();
+//       res.json(latest);
+//     } else {
+//       res.status(404).json({ error: 'Aucune position trouvée pour cet utilisateur.' });
+//     }
+//   }, {
+//     onlyOnce: true
+//   });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Serveur en ligne sur le port ${PORT}`);
+// });
+
+
+
+
+
+
+
+
+
+
 /// index.js
 const express = require('express');
 const cors = require('cors');
